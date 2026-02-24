7bff15e23d6ee4bfd86437a03d2cb434d938a0a5
SebastianOlivera <brunos.olivera@correo.ucu.edu.uy>
2026-02-24 12:26:03 -0300

    Changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from batch_processor import main
+from pozos.cli import main
 
 
 if __name__ == "__main__":
---
+++ pozos/pipeline/__init__.py
@@ -0,0 +1,3 @@
+from .batch_processor import process_folder, process_single_file
+
+__all__ = ["process_folder", "process_single_file"]
